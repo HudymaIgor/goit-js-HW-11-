18399a0634308d81adee2e1d01712c8aa8025bed
Hudyma_Ihor <Hudyma.work@gmail.com>
2022-09-18 19:13:51 +0300

    correction

--- src/index.js
+++ src/index.js
@@ -8,36 +8,37 @@ import posts from './handlebarl/posts.hbs'
 
 
 const searchForm = document.querySelector('.form');
-const searchFormBtmEl = document.querySelector('.submit');
 const galleryEl = document.querySelector('.section');
 const loadMoreBtnEl = document.querySelector('.load-more');
 
 const unsplashApi = new UnsplashApi();
 
-const lightbox = new SimpleLightbox('.gallery a');
+const lightbox = new SimpleLightbox('.photo-card a');
 
 const onSearthFormSubmit = async event => {
     event.preventDefault();
 
     unsplashApi.searchQuery = event.currentTarget.elements.searchQuery.value;
     unsplashApi.page = 1;
+loadMoreBtnEl.classList.add('is-hiden')
 
         try {
             const { data } = await unsplashApi.fetchPhotosByQuery()
+            galleryEl.innerHTML = posts(data.hits);
+            lightbox.refresh();
+
+            if (data.totalHits > unsplashApi.per_page) {
+                loadMoreBtnEl.classList.remove('is-hiden')  
+            } 
 
             if (data.totalHits === 0) { 
                 galleryEl.innerHTML = '';
                  Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
                 return 
             }      
-            if (data.totalHits > unsplashApi.per_page) {
-                loadMoreBtnEl.classList.remove('is-hiden')  
-            }
-                     if (data.totalHits = 1 ) { 
-                 Notiflix.Notify.info(`Hooray! We found ${data.total} images.`)
-            }
-            galleryEl.innerHTML = posts(data.hits);
-                lightbox.refresh();
+            
+                 Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`)
+
      } catch (err) { 
 console.log(err)
     }
@@ -46,13 +47,9 @@ const onloadMoreBtnElClick = async event => {
     
     try {
         unsplashApi.page += 1;
-        
-        
-        
         const { data } = await unsplashApi.fetchPhotosByQuery()
 
         galleryEl.insertAdjacentHTML('beforeend', posts(data.hits));
-        
               lightbox.refresh();
 
              if (data.hits.length === 0 ) { 
@@ -60,9 +57,7 @@ const onloadMoreBtnElClick = async event => {
                  loadMoreBtnEl.removeEventListener('click', onloadMoreBtnElClick);  
                  Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
                  return
-        }
-  
-        
+        }         
      } catch (err) { 
 console.log(err)
     }
